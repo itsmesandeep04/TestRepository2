39c67d3e551a6a6346f1ee2a8ca4e07193fbcba9
Sandeep Palav <sandeep.palav@objectedge.com>
2015-05-07 18:24:52 +0530

    new variable xyz in branch2

--- TestProject2/src/File1.java
+++ TestProject2/src/File1.java
@@ -10,4 +10,15 @@ public class File1 {
 	public void setAbc(String abc) {
 		this.abc = abc;
 	}
+	
+	private String xyz;
+
+	public String getXyz() {
+		return xyz;
+	}
+
+	public void setXyz(String xyz) {
+		this.xyz = xyz;
+	}
+	
 }
